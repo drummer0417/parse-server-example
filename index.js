b0fb3a6f0794edc6bd36a10621cc9575fa808721
drummer0417 <hans@androidappfactory.nl>
2016-06-06 22:02:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+   mailConfig: {
+    service: 'mailgun',
+    apiKey: process.env.MAILGUN_API_KEY || 'mg-MYMAILGUNKEY',
+    domain: process.env.MAILGUN_DOMAIN || 'mymailgundomain@mailgun.org',
+    fromAddress: process.env.MAIL_FROM_ADDRESS || 'My App <myapp@domain.com>'
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
